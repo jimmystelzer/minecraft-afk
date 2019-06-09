4abf9d16fdcf42aa16d8e0321731d398ebf44e2a
Jimmy Stelzer <jimmy.stelzer@gmail.com>
2019-06-09 11:19:18 -0300

    Update to 1.14 to prevent camera shaking when lost focus.

--- minecraft-afk.sh
+++ minecraft-afk.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-(sleep 5 && xdotool mousedown --window `xwininfo -tree -root | grep -e "(\"Minecraft [0-9]\+[-.a-z0-9]\+\" \"Minecraft [0-9]\+[-.a-z0-9]\+\")" | awk '{printf $1}'` 3) &
+PANEL_ID=`wmctrl -l | grep -e "xfce4-panel" | awk '{print $1}'`
+for I in `wmctrl -l | grep -e "Minecraft [0-9]\+[-.a-z0-9]\+" | awk '{print $1}'`; do
+	xdotool mousedown --window "$I" 3
+	sleep 2
+	xdotool mousedown --window "$I" 3
+	wmctrl -i -a $PANEL_ID
+	xdotool mousedown --window "$I" 3
+done
